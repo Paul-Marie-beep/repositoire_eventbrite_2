18e9761c5f25967477a8305887b494be6b1291e8
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-10 21:22:05 +0200

    1st commit

---
+++ app/models/event.rb
@@ -0,0 +1,37 @@
+class Event < ApplicationRecord
+  belongs_to :admin, class_name: "User"
+  has_many :attendances
+  has_many :users, through: :attendances
+  
+ 
+  validates :start_date, presence: true
+  validates :duration, presence: true
+  validates :title,  presence: true, length:  { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, inclusion: 1..1000
+  validates :location, presence: true
+
+  validate :no_start_date_in_the_past
+  validate :duration_format
+
+
+
+
+  
+
+
+  private
+   
+  def no_start_date_in_the_past
+   errors.add(:start_date, "can't be in the past") unless
+    start_date < DateTime.now
+  end
+
+  def duration_format
+    errors.add(:duration, "must be positive and a mutiple of 5") unless
+      duration > 0 && duration % 5 == 0
+  end
+
+
+end
+
