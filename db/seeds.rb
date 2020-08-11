f2d2134bf40dae399001a052c886924ab2b5fd89
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-12 00:37:45 +0200

    index suite et fin

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,15 @@ Event.destroy_all
 10.times do
 user = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.first_name, description: Faker::TvShows::Simpsons.quote,  email: Faker::Name.first_name+"@yopmail.com", password: "poumpoum")
 user.save
-puts "#{user.id} crée"
+puts "#{user.id} créé"
 end
 
 15.times do
 event =  Event.new(
   start_date: Faker::Date.in_date_period(year: 2021, month: 5),
   duration: rand(1..10)*5,
-  title: Faker::Book.title,
-  description: Faker::Hipster.sentences,
+  title: Faker::GreekPhilosophers.name,
+  description: Faker::GreekPhilosophers.quote,
   price: rand(1..1000),
   location: Faker::Address.city,
   admin: User.all.sample
