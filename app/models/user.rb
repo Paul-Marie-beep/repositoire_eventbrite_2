fb53b3217efc0e2bf8d5c7e665d773aa31b16bbe
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-11 16:39:58 +0200

    tjs svtage heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,7 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances 
   
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :description, presence: true, length: { minimum: 10 }
-
+ 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
