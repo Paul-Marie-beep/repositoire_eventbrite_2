d8e0e9d09c41cea790d8b817b064bda27c981f16
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-12 16:11:08 +0200

    ajustement création événement

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,6 @@ class EventsController < ApplicationController
     @new_event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
     @new_event.admin = current_user
     @new_event.save
-    
   end
 
 
