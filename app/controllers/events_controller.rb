c0c5bbb9204fc8d88542ee6aa15822fcbf16ba54
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-12 16:41:00 +0200

    flash créa événement

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,13 +8,19 @@ class EventsController < ApplicationController
   end
 
   def new 
-    @new_event = Event.new
+    @new_event = Event.new #il faut créer l'élément pour que le formulaire fonctionne
   end
 
   def create
+    @all_events = Event.all #Vu qu'on renvoie sur l'index si la création marche il faut initialiser @all_events
+
     @new_event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
     @new_event.admin = current_user
-    @new_event.save
+      if @new_event.save
+        render 'index.html.erb'
+      else
+        render 'new.html.erb'
+      end
   end
 
 
