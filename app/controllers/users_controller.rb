7fb4e7c538c6e671bd72e251c27451883f9489f0
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-12 14:45:45 +0200

    page modif mail/password user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-
-    if current_user == @user
+ 
+    #if current_user == @user
       @user_events = Event.where(admin_id: @user.id)
-    else
-      redirect_to root_path
-    end
+    #else
+    #  redirect_to root_path
+    #end
   end
 
 end
