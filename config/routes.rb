c21fea86810919f14b56c96230968eb6a2ec0ea0
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-12 11:13:52 +0200

    modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'events/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :events
+
+  #On ne veut pas que ça en fasse trop pour se culbuter avec les trucs de Devise
+  resources :users, only: [:show]
 
   root 'events#index'
 end
