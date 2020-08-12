fe61e806c3e740a463e0f7d86cf56b9665499d45
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-12 15:56:01 +0200

    page création d'événement

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,15 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
-  def new
+  def new 
+    @new_event = Event.new
+  end
+
+  def create
+    @new_event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
+    @new_event.admin = current_user
+    @new_event.save
+    
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
  
-    #if current_user == @user
+    if current_user == @user
       @user_events = Event.where(admin_id: @user.id)
-    #else
-    #  redirect_to root_path
-    #end
+    else
+     redirect_to root_path
+    end
   end
 
 end
