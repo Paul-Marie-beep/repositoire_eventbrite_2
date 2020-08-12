ccc66066a8b96a6cd7e82a47a6f6f40c4b5bb159
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-12 13:01:44 +0200

    page user hideuse mais fonctionnelle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @user_events = Event.where(admin_id: @user.id)
 
+    if current_user == @user
+      @user_events = Event.where(admin_id: @user.id)
+    else
+      redirect_to root_path
+    end
   end
 
 end
