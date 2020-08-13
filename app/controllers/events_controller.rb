f0fa260cabd51a14561ea18827fadf2300e71262
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-13 10:23:40 +0200

    modif pour users non connectés

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,12 +37,14 @@ class EventsController < ApplicationController
 
   #Vérifier que quelqu'un qui est connecté sur le site ne participie pas déjà à l'événement dont il regarde la page
   def not__a_participant(event)
-    #Attendance, c'est la table de jointure users/events —> On cherche les attendances qui font correspondre le personne connectée avec les événéments qu'il regarde
-      att_list = Attendance.where(event_id: @event.id, user_id: current_user.id)
-    #C'est un tableau: s'il est vide, alors c'est que le current_user ne participe pas à l'événement en question
-    if att_list == []
-      return true
-    end
+    if current_user  #Pour pas casser le site si on veut voir une page en étant pas connecté (dans l'idée, il faudrait pouvoir cliquer sur le bouton en étant pas connecté mais on verra après)
+      #Attendance, c'est la table de jointure users/events —> On cherche les attendances qui font correspondre le personne connectée avec les événéments qu'il regarde
+        att_list = Attendance.where(event_id: @event.id, user_id: current_user.id)
+      #C'est un tableau: s'il est vide, alors c'est que le current_user ne participe pas à l'événement en question
+      if att_list == []
+        return true
+      end
+    end  
   end
 
 
