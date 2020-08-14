4baaad4bf56b0af264e588110e27bec555857b9a
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-14 18:01:15 +0200

    possibilité ajout photo

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :admin, class_name: "User"
   has_many :attendances
   has_many :users, through: :attendances
+  has_one_attached :event_picture
   
  
   #validates :start_date, presence: true
