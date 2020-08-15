e6f0a019b5e16dddefa21a0b80d60426c69cdb41
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2020-08-15 12:17:57 +0200

    ajout pp possible | pp générique sinon | Comment imposer une taille?

--- app/controllers/profile_pictures_controller.rb
+++ app/controllers/profile_pictures_controller.rb
@@ -1,4 +1,12 @@
 class ProfilePicturesController < ApplicationController
+ 
+ 
+ 
   def create
+      @user = User.find(params[:user_id])
+      @user.profile_picture.attach(params[:profile_picture])
+      redirect_to(user_path(@user))
   end
+
+
 end
